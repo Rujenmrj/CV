ec7e502d73e57534c507c2ad8ff383bdd8be3048
rujen mrj <rujenmrj15@gmail.com>
2023-07-15 18:18:03 +0545

    created snake surface then changed the snake, food and bg color

--- game/snake.py
+++ game/snake.py
@@ -1,5 +1,14 @@
 import pygame
 import sys
+from pygame.math import Vector2
+import random
+
+
+class snake:
+    def __init__(self):
+        self.x = 5
+        self.y = 2
+        self.pos = Vector2(snake.x, snake.y)
 
 pygame.init()
 WIDTH, HEIGHT = 30, 20
@@ -8,7 +17,7 @@ clock = pygame.time.Clock()
 
 while True:
 
-    Window.fill((0, 50, 50))
+    Window.fill((0, 75, 25))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
