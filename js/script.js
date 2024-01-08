afd996186a090501de5cf707936eb9ccb9fd8c05
rujenmrj <rujenmrj15@gmail.com>
2024-01-08 16:09:57 +0545

    removed project preview from index, added skillbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const animate=document.querySelectorAll('.showanimation');
-window.onload=()=>{
-    animate.classList.add('active');
-}
+progressBarh=document.querySelector(".loadbarhtml");
+progressBarc=document.querySelector(".loadbarcss");
+progressBarj=document.querySelector(".loadbarjs");
+var observer = new IntersectionObserver(function(entries) {
+    if(entries[0].isIntersecting === true) {
+        entries[0].target.style.width=entries[0].target.innerHTML;
+    } else {
+        entries[0].target.style.width=0;
+    }
+}, { threshold: [0] });
+
+observer.observe(progressBarh);
+observer.observe(progressBarc);
+observer.observe(progressBarj);
